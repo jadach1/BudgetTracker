e49c9285b93f17e1ac9416d9d2a328f487422798
JAdach1 <jacobadach@live.ca>
2024-06-10 20:09:43 +0100

    d

--- obj/Debug/net8.0/Budget Man.AssemblyInfo.cs
+++ obj/Debug/net8.0/Budget Man.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829784b589c657feb30b2c4d107fe58542f22e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cafeedf2b45ceae9f895d2d79f16fb411ae2ab58")]
 [assembly: System.Reflection.AssemblyProductAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
