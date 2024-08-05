46230f50c71a02809ef0657202ddb6834d29ac97
JAdach1 <jacobadach@live.ca>
2024-08-05 19:53:16 -0400

    added functinoality to select a currency.  Currency will persist in memory and be displayed on screen

--- obj/Debug/net8.0/Budget Man.AssemblyInfo.cs
+++ obj/Debug/net8.0/Budget Man.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a6592eebf2a78c66d222b95481e1dbc0f8c30a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8318588b22e0264f09839d4f1cd1b4427b27782b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,26 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+console.log("poof, reset!")
+
+console.log(window.localStorage.getItem("currency"));
+
 var currency_formatter = new Intl.NumberFormat('en-US',{
     style: 'currency',
-    currency: 'USD'
+    currency:  window.localStorage.getItem('currency')
 })
+
+document.getElementById("currency_selected").innerHTML = window.localStorage.getItem('currency');
+
+function currencyChange() {
+    const elem = document.getElementById("currencySelection");
+    currency_formatter = new Intl.NumberFormat('en-US',{
+        style: 'currency',
+        currency: elem.value
+    })
+    window.localStorage.setItem('currency', elem.value);
+    document.getElementById("currency_selected").innerHTML = elem.value ;
+}
 
 //Format 2024-5-2 to Thursday May 2024
 var listOfMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -15,5 +31,18 @@ function humanReadableDate(date){
     let day = listOfDays[d.getDay()];
     let year = d.getFullYear();
     return day+" "+month+" "+year;
+}
+
+function screenSizeCheck(){
+    const size = +document.getElementsByTagName("body")[0].clientWidth;
+    const height = +document.getElementsByTagName("body")[0].clientHeight;
+    const bbody = document.getElementsByTagName("body");
+    
+    console.log("width " + size);
+   console.log("height " + height);
+   console.log(bbody);
+
+   const windowHeight = window.innerHeight;
+   console.log("height " + windowHeight);
 }
 
