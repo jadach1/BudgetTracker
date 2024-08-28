69fb08581e30f5a0c896e9818635e52c19e5dc78
JAdach1 <jacobadach@live.ca>
2024-08-28 13:02:48 -0400

    Completed the hide button functionality for the Expense Table Modal.  Expenses can now be Edited, Deleted, from a modal which appears on a row within the table of expenses the user has selected.  And will Transport itself around different rows where the user clicks, and will successfully hide itself while removing the appropriate CSS from the table-row

--- obj/Debug/net8.0/Budget Man.AssemblyInfo.cs
+++ obj/Debug/net8.0/Budget Man.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b61a49c9f398810393a6d777cbce779712446b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b07428d588c53eb775c15a8877465a5157bdc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Budget Man")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
